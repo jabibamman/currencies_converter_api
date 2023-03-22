17f7c2d531636fd4985957271f90ddecad5b7167
James ABIB <james.bidule@gmail.com>
2023-03-22 21:35:24 +0100

    deleted RMB2EUR

--- src/api/controllers/api.controller.ts
+++ src/api/controllers/api.controller.ts
@@ -6,17 +6,12 @@ import { ApiService } from '../services/api.service';
 export class ApiController {
     constructor(private readonly apiService: ApiService, private readonly currencyServices: CurrencyService) {}
 
-    @Get('amount/:amount')
-    async RMB2EUR(@Param('amount') amount: number): Promise<string> { 
-      return this.apiService.RMB2EUR(amount);
-    }
-
     @Get('currencies')
     async getAllCurrencies(): Promise<string[]> {
         return await this.currencyServices.fetchCurrencyValues();
     }
 
-    // cur to cur conversion
+    // "from" to "to" conversion
     @Get('convert/:amount/:from/:to')
     async convert(@Param('amount') amount: number, @Param('from') from: string, @Param('to') to: string): Promise<string> {
         return this.apiService.convert(amount, from, to);
--- src/api/services/api.service.ts
+++ src/api/services/api.service.ts
@@ -4,30 +4,6 @@ import * as cheerio from 'cheerio';
 
 @Injectable()
 export class ApiService {
-    async RMB2EUR(amount: number): Promise<any> {
-        const url = `https://wise.com/fr/currency-converter/cny-to-eur-rate?amount=${amount}`;
-        const response = await axios.get(url);
-        const html = response.data; 
-        const $ = cheerio.load(html);
-        let targetInputValue: string;
-    
-        $('.form-group > div').each((_, element) => {
-          const text = $(element).text().trim();
-          if (text.toLowerCase().includes('eur')) {
-            // replace eur by €
-            targetInputValue = text.replace(/(eur|EUR)/g, '€');
-            return false; 
-          }
-        });
-             
-          return {
-            from: 'CNY',
-            to: 'EUR',
-            amount: amount,
-            result: targetInputValue,
-          }
-      }
-
       async convert(amount: number, from: string, to: string): Promise<any> {
         const url = `https://wise.com/fr/currency-converter/${from}-to-${to}-rate?amount=${amount}`;
         const response = await axios.get(url);
