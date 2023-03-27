e41ba8da0dffed89093947f2f6dbd9462f722baa
James ABIB <james.bidule@gmail.com>
2023-03-27 10:24:28 +0200

    feat: add nestjs swagger

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,22 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
-import { CurrencyService } from './currency/services/currency.service';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const packageJson = require('../package.json');
+  const config = new DocumentBuilder()
+  .setTitle(packageJson.name.replace(/_/g, ' ').replace(/\w\S*/g, (txt) => txt.charAt(0).toUpperCase() + txt.substr(1)))
+  .setDescription(packageJson.description)
+  .setVersion(packageJson.version)
+  .setContact(packageJson.author.name, packageJson.author.url, packageJson.author.email)
+
+  packageJson.tags.forEach(tag => {
+    config.addTag(tag);
+  });
+
+  const document = SwaggerModule.createDocument(app, config.build());
+  SwaggerModule.setup('api', app, document);
   app.enableCors();
   await app.listen(3000);
 }
